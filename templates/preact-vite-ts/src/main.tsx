1ad636d3aa8977db479006f4904e92e2b0da7ec8
Jeroen Claassens <support@favware.tech>
2024-07-27 10:37:39 +0200

    chore: it's possible! you can turn a 50-line code chunk into just 3 lines. here's how

--- templates/preact-vite-ts/src/main.tsx
+++ templates/preact-vite-ts/src/main.tsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import './index.css';
 
-render(<App />, document.getElementById('app')!)
+import { render } from 'preact';
+import App from './app.tsx';
+
+render(<App />, document.getElementById('app')!);
