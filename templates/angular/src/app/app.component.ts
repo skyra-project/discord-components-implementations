a216fb2d0f3f40b46825bc0051e65a31907f491e
Jeroen Claassens <support@favware.tech>
2024-12-01 13:16:01 +0100

    fix: update angular to v19
    closes #68

--- templates/angular/src/app/app.component.ts
+++ templates/angular/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import '@skyra/discord-components-core';
 
 @Component({
 	selector: 'app-root',
-	standalone: true,
-	imports: [RouterOutlet],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	templateUrl: './app.component.html'
 })
